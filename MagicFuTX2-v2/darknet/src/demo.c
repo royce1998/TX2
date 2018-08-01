84cca827ccb903c766f6edbb978d26d38ae9c02d
Royce Yang <royce1.yang@partner.midea.com>
2018-08-01 16:08:23 -0700

    uncommented sleep

--- MagicFuTX2-v2/darknet/src/demo.c
+++ MagicFuTX2-v2/darknet/src/demo.c
@@ -190,7 +190,7 @@ void *fetch_loop(){
         display_in_thread(0);
         fetch_in_thread(0);
         // sleep(detect_time * 0.8);
-        printf("\n%.3f\n", detect_time);
+        // printf("\n%.3f\n", detect_time);
         last_detect_time = detect_time;
         // pthread_mutex_unlock(&lock);
     }
