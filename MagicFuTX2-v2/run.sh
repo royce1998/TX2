05a5ea2b004da58e31220f85ff6d5f648892e9d0
Royce Yang <royce1.yang@partner.midea.com>
2018-08-01 15:37:57 -0700

    debug

--- MagicFuTX2-v2/run.sh
+++ MagicFuTX2-v2/run.sh
@@ -2,7 +2,8 @@
 
 killall -9 darknet
 cd darknet
-cp box_coordinate.txt box_coordinate_previousrun.txt
-rm box_coordinate.txt
+# cp box_coordinate.txt box_coordinate_previousrun.txt
+# rm box_coordinate.txt
+make
 ./darknet detector demo cfg/coco.data cfg/yolov2-tiny.cfg ../yolov2-tiny.weights -thresh 0.4
 cd ../
