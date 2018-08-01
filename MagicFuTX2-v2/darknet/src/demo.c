1ef9b262baa09f504258796b53c6191f6c58032f
Royce Yang <royce1.yang@partner.midea.com>
2018-08-01 15:50:16 -0700

    sleep less

--- MagicFuTX2-v2/darknet/src/demo.c
+++ MagicFuTX2-v2/darknet/src/demo.c
@@ -181,14 +181,15 @@ void *detect_loop(void *ptr)
 }
 
 void *fetch_loop(){
+    double last_detect_time = 0.0;
     while(!demo_done){
         // pthread_mutex_lock(&lock);
         buff_index = (buff_index + 1) % 3;
         fps = 1./(what_time_is_it_now() - demo_time);
         demo_time = what_time_is_it_now();
-        display_in_thread(0);
+        //display_in_thread(0);
         fetch_in_thread(0);
-        sleep(detect_time);
+        sleep(detect_time * 0.8);
         printf("\n%.3f\n", detect_time);
         // pthread_mutex_unlock(&lock);
     }
