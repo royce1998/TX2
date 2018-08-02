fd0820d1a7dd5e0b096ce6ec9e869740fb434bd2
Royce Yang <royce1.yang@partner.midea.com>
2018-08-01 17:29:29 -0700

    image.c change

--- MagicFuTX2-v2/darknet/src/image.c
+++ MagicFuTX2-v2/darknet/src/image.c
@@ -585,7 +585,7 @@ void draw_bbox_info(image im, int class, int classes, box bbox, char *info, char
         {
             fprintf(box_coordinate, "\n\n[Object Info] of [%s] : \n\tBox Coordinates : (%d,%d), (%d,%d)",info,left,top,right,bot);
         }
-        if(status != '\0') fprintf(box_coordinate, "\nStatus : %s", status);
+        if(alphabet && status != 0) fprintf(box_coordinate, "\nStatus : %s", status);
         else error("File not opened");
 
         fclose(box_coordinate);
